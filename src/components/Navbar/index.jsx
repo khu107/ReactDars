a36ba8e64fe3eea46b3a184922c2e55238dd037a
Khusan <khu10735@naver.com>
2022-06-30 01:40:33 +0900

    minus

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,12 +3,16 @@ import { useState } from "react"
 const HooksComp = ()=>{
     const [count,setCount] = useState(0); // useState ardoim array qaytaradi
     console.log(count);
+
+    const minus = ()=>{
+        count < 1 ? setCount(count) : setCount(count - 1)   
+    }
     return(
         <div>
             <h1>Hooks Componont</h1>
             <h1>Count: {count}</h1>
             <button onClick={()=>setCount(count + 1)}>+</button>
-            <button onClick={()=>setCount(count - 1)}>-</button>
+            <button onClick={minus}>-</button>
         </div>
     )
 }
