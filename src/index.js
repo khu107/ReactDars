9e9b089f8f85a777f2f4c6f36ada957da9ecf133
Khusan <khu10735@naver.com>
2022-07-16 09:43:40 +0900

    route5

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Root from "./root";
-import Context from "./context";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // render ekranga chiqarish degani
   // React.StrictMode birxil eski kutubxonalar bulsa consolda ogohlantiradi
   <React.StrictMode>
-    <Context>
+    <BrowserRouter>
       <Root />
-    </Context>
+    </BrowserRouter>
   </React.StrictMode>
 );
--- src/root/index.jsx
+++ src/root/index.jsx
@@ -1,20 +1,45 @@
-import React, { Component } from "react";
-import { BrowserRouter } from "react-router-dom";
-import Navbar from "../components/Navbar";
-import Students from "../components/Students";
-import Context from "../context";
+// import React, { Component } from "react";
 
-export default class Root extends Component {
-  render() {
-    return (
-      <React.StrictMode>
-        <BrowserRouter>
-          <Context>
-            <Navbar />
-            <Students />
-          </Context>
-        </BrowserRouter>
-      </React.StrictMode>
-    );
-  }
+// export default class Root extends Component {
+//   render() {
+//     return <React.StrictMode></React.StrictMode>;
+//   }
+// }
+
+import React from "react";
+import "../index.css";
+
+import { Switch, Route, NavLink } from "react-router-dom";
+import Home from "../Home";
+import About from "../About";
+import Contact from "../Contact";
+import ErrorPage from "../ErrorPage";
+import Footer from "../Footer";
+export default function Root() {
+  const activeStyle = {
+    textDecoration: "none",
+    color: "white",
+  };
+  return (
+    <div>
+      <nav className="nav">
+        <NavLink exact to={"/"} activeStyle={activeStyle}>
+          Home
+        </NavLink>
+        <NavLink to={"/about"} activeStyle={activeStyle}>
+          About
+        </NavLink>
+        <NavLink to={"/contact"} activeStyle={activeStyle}>
+          Contact
+        </NavLink>
+      </nav>
+      <Switch>
+        <Route exact path={"/"} component={Home} />
+        <Route path={"/about"} component={About} />
+        <Route path={"/contact"} component={Contact} />
+        <Route path={"*"} component={ErrorPage} />
+      </Switch>
+      <Footer />
+    </div>
+  );
 }
